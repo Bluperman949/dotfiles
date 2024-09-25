57306f9a0ea0ee910df679cf9207d0263fc944dd
Bluperman949 <christiandoolittle@hotmail.com>
2024-09-24 18:43:16 -0700

    nvim f5 to run

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -3,3 +3,15 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
 vim.keymap.set('n', 'gr', vim.lsp.buf.rename, {})
 vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, {})
+
+-- Terminal
+local ftruns = {
+  python = 'python %'
+}
+vim.keymap.set('n', '<F5>', function ()
+  local cmd = ftruns[vim.bo.filetype]
+  vim.cmd('silent !kitty @launch --type=overlay --cwd=current')
+  if (cmd) then
+    vim.cmd('silent !kitty @send-text --match state:active '..cmd)
+  end
+end)
