6e335bd4a29a428eb2f35f16ec91c986dfada3bd
Bluperman949 <christiandoolittle@hotmail.com>
2024-12-16 21:09:06 -0800

    etc install.sh

---
+++ etc/install_etc.sh
@@ -0,0 +1,3 @@
+sudo ln -s "$PWD/90-touchpad.conf" '/etc/X11/xorg.conf.d/90-touchpad.conf'
+sudo ln -s "$PWD/lightdm.conf" '/etc/lightdm/lightdm.conf'
+sudo ln -s "$PWD/mybash.sh" '/etc/profile.d/mybash.sh'
