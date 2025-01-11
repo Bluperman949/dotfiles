6d3002a270fa5890f3011bfd44f03f71aab81356
Bluperman949 <christiandoolittle@hotmail.com>
2025-01-11 14:07:40 -0800

    deleted old nvim files

--- nvim/.config/nvim/lua/keybinds.lua
+++
@@ -1,7 +0,0 @@
--- LSP
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-vim.keymap.set('n', 'gr', vim.lsp.buf.rename, {})
-vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, {})
-vim.keymap.set('n', 'gu', vim.lsp.buf.references, {})
-vim.keymap.set('n', 'g<space>', vim.lsp.buf.hover, {})
