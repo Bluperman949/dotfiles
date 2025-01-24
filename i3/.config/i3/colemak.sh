c02a4a57bf480a716eeb1f510a1e8cf95121450e
Bluperman949 <christiandoolittle@hotmail.com>
2025-01-23 16:56:45 -0800

    colemak.sh i3

---
+++ i3/.config/i3/colemak.sh
@@ -0,0 +1,8 @@
+#
+#  colemak.sh
+#  toggle colemak us layout via `setxkbmap` 
+#
+
+[[ $(setxkbmap -print | grep -oP 'colemak') ]] \
+  && setxkbmap -layout us \
+  || setxkbmap -layout us -variant colemak \
