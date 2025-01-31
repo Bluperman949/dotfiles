90c7228574e7a2648724f48c977e8f4cec0719d7
Bluperman949 <christiandoolittle@hotmail.com>
2025-01-31 12:04:35 -0800

    slight rework of leap/fzf keymaps

--- nvim/.config/nvim/lua/my/plugins/navigation.lua
+++ nvim/.config/nvim/lua/my/plugins/navigation.lua
@@ -10,17 +10,14 @@ return {
       local fzf = require'fzf-lua'
       fzf.setup{
         'fzf-native',
-        keymap = {
-          ['<A-q>'] = 'hide',
-        },
       }
 
       map({'n', 'v'}, '<C-f>', '<nop>')
       map({'n', 'v'}, '<C-f><C-f>', fzf.blines)
-      map({'n', 'v'}, '<C-f><C-b>', fzf.buffers)
+      map({'n', 'v'}, '<C-f><C-r>', fzf.buffers)
       map({'n', 'v'}, '<C-f><C-o>', fzf.oldfiles)
       map({'n', 'v'}, '<C-f><C-i>', fzf.files)
-      map({'n', 'v'}, '<C-S-f>', fzf.builtin)
+      map({'n', 'v'}, '<C-f><C-b>', fzf.builtin)
 
       map({'n', 'v'}, 'gu', fzf.lsp_references)
       map({'n', 'v'}, 'gd', fzf.lsp_definitions)
@@ -31,7 +28,7 @@ return {
     'ggandor/leap.nvim',
     event = 'VeryLazy',
     config = function()
-      map({ 'n', 'x', 'o' }, 'ff', '<Plug>(leap)')
+      map('n', 'ff', '<Plug>(leap)')
     end,
   },
 }
