bd85333c366131e0c83c6db0764aa10f30cfeb57
Bluperman949 <christiandoolittle@hotmail.com>
2025-03-13 19:54:28 -0700

    update setup.sh, packages.txt

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ sudo gpasswd -a chris autologin
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
 makepkg -si
+
 cd ~/.dotfiles
-sudo cp -r etc/* /etc/
-
 yay -S - < packages.txt
-
-ls -d */ | stow -v
