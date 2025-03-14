81f82a98e6f84b5b86b87e2f5326df071a9ba765
Bluperman949 <christiandoolittle@hotmail.com>
2025-03-13 19:39:58 -0700

    half-finished treesitter rework

--- nvim/.config/nvim/lua/my/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/my/plugins/treesitter.lua
@@ -1,22 +1,48 @@
 local TS_LANGS = {
-  'vim', 'lua', 'python', 'bash', 'java', 'groovy', 'zig',
+  'vim', 'lua', 'python', 'html',
 }
 
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter-textobjects' },
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter' },
     event = 'VeryLazy',
+    build = ':TSUpdate',
+    config = function ()
+      require'nvim-treesitter.configs'.setup{
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymps = {
+              ['ip'] = '@parameter.inner',
+              ['ap'] = '@parameter.outer',
+              ['in'] = '@number.inner',
+              ['an'] = '@number.outer',
+              ['if'] = '@function.inner',
+              ['af'] = '@function.outer',
+            },
+          },
+        },
+      }
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    -- event = 'VeryLazy',
     config = function()
       require'nvim-treesitter.configs'.setup{
         ensure_installed = TS_LANGS,
         highlight = {
           enable = true,
         },
+        indent = {
+          enable = false,
+        },
+        incremental_selection = {
+
+        }
       }
     end
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
   },
 }
