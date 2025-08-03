cce294388d2aec85a1378338fbac89bb7bf00ebe
Bluperman949 <christiandoolittle@hotmail.com>
2025-08-03 12:19:42 -0700

    change colemak to colemak_dh

--- i3/colemak.sh
+++ i3/colemak.sh
@@ -1,8 +1,8 @@
 #
 #  colemak.sh
-#  toggle colemak us layout via `setxkbmap` 
+#  toggle colemak us layout via `setxkbmap`
 #
 
 [[ $(setxkbmap -print | grep -oP 'colemak') ]] \
   && setxkbmap -layout us \
-  || setxkbmap -layout us -variant colemak \
+  || setxkbmap -layout us -variant colemak_dh \
