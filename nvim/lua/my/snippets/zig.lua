1e429c25eb0d3797adf4675849fe94feb489b347
Bluperman949 <christiandoolittle@hotmail.com>
2025-08-03 12:21:25 -0700

    nvim add whii snippet for zig

--- nvim/lua/my/snippets/zig.lua
+++ nvim/lua/my/snippets/zig.lua
@@ -1,7 +1,15 @@
 local _ENV = require'my.snippets._defs'
 
+local function copy(args, parent, uargs)
+  return args[1];
+end
+
 return {
-  s('var', {
-    t'var ', i(1, 'name'), t''
+  s('whii', {
+    t'while (',
+      i(1, 'i'), t' < ', i(2, 'len'),
+    t') : (',
+      f(copy, {1}),
+    t' += 1) {', t'}'
   }),
 }
