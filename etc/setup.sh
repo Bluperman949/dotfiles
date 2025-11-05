6f48ca2947cf3a37059da8e5d709344f0b81285a
Bluperman949 <christiandoolittle@hotmail.com>
2025-11-04 17:33:01 -0800

    fcitx

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,2 +1,3 @@
 sudo ln -s "$PWD/90-touchpad.conf" '/etc/X11/xorg.conf.d/90-touchpad.conf'
 sudo ln -s "$PWD/mybash.sh" '/etc/profile.d/mybash.sh'
+sudo ln -s "$PWD/fcitx.sh" '/etc/profile.d/fcitx.sh'
