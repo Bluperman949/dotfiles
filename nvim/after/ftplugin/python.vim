7946b7254bb8bd0d9c96d563dd9e6f2501dbda65
Bluperman949 <christiandoolittle@hotmail.com>
2026-02-06 23:35:20 -0800

    nvim stupid conflict fixes

--- nvim/after/ftplugin/python.vim
+++ nvim/after/ftplugin/python.vim
@@ -2,4 +2,4 @@ set tabstop=2
 set shiftwidth=2
 set expandtab
 
-nno ,, <cmd>term python %<cr>
+nno ,/ <cmd>term python %<cr>
--- nvim/lua/my/plugins/maker.lua
+++ nvim/lua/my/plugins/maker.lua
@@ -4,7 +4,7 @@ return {
     event = 'VeryLazy',
     config = function ()
       require'maker'.setup{
-        window_command = 'tabnew | term'
+        window_command = 'tabnew | term',
       }
     end
   }
